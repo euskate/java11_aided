f7fbe5c0db88bb18a9eb74e7c7a0d18138c1299b
kkt09072 <ksb450424@naver.com>
2024-03-13 11:01:04 +0900

    java11 oracle connection test

---
+++ test19/src/org/kh/database/OracleExam2.java
@@ -0,0 +1,58 @@
+package org.kh.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class OracleExam2 {
+	public static void main(String[] args) {
+		Connection con = null;	//연결
+		PreparedStatement pstmt = null;	//상태 변경(ON/OFF) 하고, SQL 문장 실행
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String userid = "system";
+		String userpw = "1234";
+		String sql = "insert into student values(?,?,?)";
+		Student st = new Student(7, "조우진", 95);
+		int i = 0;
+		try {
+			Class.forName(driver);
+			try {
+				con = DriverManager.getConnection(url, userid, userpw);
+				pstmt = con.prepareStatement(sql);
+				pstmt.setInt(1, st.getNo());
+				pstmt.setString(2, st.getName());
+				pstmt.setInt(3, st.getPoint());
+				i = pstmt.executeUpdate();
+				if(i>0) {
+					System.out.println(i+"건이 성공적으로 처리 되었습니다.");
+				} else {
+					System.out.println("SQL 구문 실패");
+				}
+			} catch (SQLException e) {
+				System.out.println("데이터베이스 연결 실패 또는 SQL 처리 실패");
+				e.printStackTrace();
+			}
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패");
+			e.printStackTrace();
+		} finally {
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(con!=null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
